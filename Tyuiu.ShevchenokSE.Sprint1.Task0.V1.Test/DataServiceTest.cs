7e5f78804fc13c4ad99ae681b4c7c0fca7116fb0
Nagibator2007 <shevchenokse@outlook.com>
2025-10-30 12:11:21 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ShevchenokSE.Sprint1.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShevchenokSE.Sprint1.Task0.V1.Lib;
+
+namespace Tyuiu.ShevchenokSE.Sprint1.Task0.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint1.Task0.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShevchenokSE.Sprint1.Task0.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShevchenokSE.Sprint1.Task0.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0453ad6-c8a9-45b9-9b44-9fb66801a768")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
