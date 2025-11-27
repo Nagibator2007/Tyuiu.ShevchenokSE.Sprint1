bfa424c999f15ad260d4911fa2e59c29908fb489
Nagibator2007 <shevchenokse@outlook.com>
2025-11-27 20:47:33 +0500

    task3 fix

---
+++ Tyuiu.ShevchenokSE.Sprint1.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ShevchenokSE.Sprint1.Task3.V1.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint1.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExspression()
+        {
+            DataService ds = new DataService();
+            double r = 5;
+            double h = 11;
+            double wait = 863.5;
+            var res = ds.CylinderVolume(r, h);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint1.Task3.V1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ShevchenokSE.Sprint1.Task3.V1.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint1.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Шевченок С.Е. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич | СМАРТб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет по формуле p * r^2 * h  результат печатает его на экран       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double r, h;
+            Console.WriteLine("Введите значение Радиус :");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Высота :");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CylinderVolume(r, h) + " Объем цилиндра");
+            Console.ReadLine();
+        }
+    }
+}
