c433d863c38c3f9ccdb75d4f25df8381187cd734
Nagibator2007 <shevchenokse@outlook.com>
2025-11-27 22:09:21 +0500

    task6 fix

---
+++ Tyuiu.ShevchenokSE.Sprint1.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ShevchenokSE.Sprint1.Task6.V15.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint1.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCheckLettersCount()
+        {
+            DataService ds = new DataService();
+            string value = "stroka...";
+            bool result = ds.CheckLettersCount(value);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint1.Task6.V15/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.ShevchenokSE.Sprint1.Task6.V15.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint1.Task6.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Шевченок С.Е. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Работа со строками класс String                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич | СМАРТб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что в строке  *");
+            Console.WriteLine("* Больше букв, чем знаков.                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите строку: ");
+            string value = Console.ReadLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            var result = ds.CheckLettersCount(value);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
